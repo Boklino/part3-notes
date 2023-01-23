9ce168b7fb47910971887d4539bc140afb1fee10
Youssef Albokl <88749793+Boklino@users.noreply.github.com>
2023-01-23 10:59:10 +0200

    commit

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true,
-  },
-];
+const Note = require("./models/note");
 
 app.use(express.json());
 app.use(express.static("build"));
@@ -45,20 +26,14 @@ app.post("/api/notes", (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
-});
-
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -69,14 +44,15 @@ app.delete("/api/notes/:id", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
+});
+
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
